5e76afc7a9ee8ac9ee799df6b34beb6583243667
tcrsilva <tais.crsilva@hotmail.com>
2018-09-27 21:08:22 -0300

    Added views to create and edit blog post INSIDE the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url
+from django.urls import path 
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list'),
-	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
